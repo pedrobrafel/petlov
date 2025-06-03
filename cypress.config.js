6ec281967de30db5663c75b46f9f5fff7a4bdb3c
Pedro Felix <pb.felix@hotmail.com>
2025-06-03 10:46:33 -0300

    adicionando github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  projectId: "3zceue",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
     baseUrl: 'https://petlov.vercel.app'
   },
